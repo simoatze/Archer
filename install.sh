b9c2f5929b7e6042072aba0accab6f8baefd6373
Simone Atzeni <atzeni1@llnl.gov>
2015-05-26 09:04:27 -0700

    Fixed installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# set -e
 # set -x
 
 if [ "$(uname)" == "Linux" ]; then
@@ -13,7 +14,9 @@ echoc() { echo -e "${RED}$1${NC}"; }
 
 LLVM_INSTALL=/usr
 HTTP=false
+LLVM_TRUNK=false
 
+# CC and CXX
 for i in "$@"
 do
     case $i in
@@ -25,8 +28,12 @@ do
 	    HTTP=true
 	    shift
 	    ;;
+	--llvm-trunk)
+	    LLVM_TRUNK=true
+	    shift
+	    ;;
 	*)
-	    echo "Usage: ./install.sh [--prefix=PREFIX[/usr]]"
+	    echo "Usage: ./install.sh [--prefix=PREFIX[/usr] [--http (to use HTTP git url)] [--llvm-trunk (install the last version of LLVM/Clang, currently supports only OpenMP 3.1)]"
 	    exit
 	    ;;
     esac
@@ -56,13 +63,58 @@ BASE=`pwd`/LLVM
 mkdir -p ${BASE}
 cd $BASE
 
+# Software Repositories
+LLVM_REPO=""
+CLANG_REPO=""
+LLVMRT_REPO=""
+POLLY_REPO=""
+LLVM_COMMIT=""
+LLVMRT_COMMIT=""	 
+CLANG_COMMIT=""
+POLLY_COMMIT=""
+REPO_RESET=false
+if [ "$HTTP" == "true" ]; then
+    if [ "$LLVM_TRUNK" == "true" ]; then
+	LLVM_REPO="https://github.com/clang-omp/llvm_trunk"
+	CLANG_REPO="https://github.com/clang-omp/clang_trunk"
+	LLVMRT_REPO="https://github.com/clang-omp/compiler-rt_trunk"
+	POLLY_REPO="https://github.com/llvm-mirror/polly.git"
+    else
+	LLVM_REPO="https://github.com/clang-omp/llvm.git"
+	CLANG_REPO="-b clang-omp https://github.com/clang-omp/clang.git"
+	LLVMRT_REPO="https://github.com/clang-omp/compiler-rt.git"
+	POLLY_REPO="https://github.com/llvm-mirror/polly.git"
+	REPO_RESET=true
+    fi
+else
+    if [ "$LLVM_TRUNK" == "true" ]; then
+	LLVM_REPO="git@github.com:clang-omp/llvm_trunk.git"
+	CLANG_REPO="git@github.com:clang-omp/clang_trunk.git"
+	LLVMRT_REPO="git@github.com:clang-omp/compiler-rt_trunk.git"
+	POLLY_REPO="git@github.com:llvm-mirror/polly.git"
+    else
+	LLVM_REPO="git@github.com:clang-omp/llvm.git"
+	CLANG_REPO="-b clang-omp git@github.com:clang-omp/clang.git"
+	LLVMRT_REPO="git@github.com:clang-omp/compiler-rt.git"
+	POLLY_REPO="git@github.com:llvm-mirror/polly.git"
+	REPO_RESET=true
+    fi
+fi
+
+# Software Version
+if [ "$REPO_RESET" == "true" ]; then
+    LLVM_COMMIT="e45b045553e027cbe400cbb8ac8c264abbbfaf83"
+    LLVMRT_COMMIT="5ebcef4a170007700aa01e4561fc802b951e5951" 
+    CLANG_COMMIT="b2fc5b326873b2f3bb919d11fe7f74981b517a79"
+    POLLY_COMMIT="d24789b6ad270d80509c73fc986d1275895c5b7b"
+fi
+
 # LLVM installation directory
 ARCHER_INSTALL=${LLVM_INSTALL}/local/archer
 LLVM_SRC=${BASE}/llvm_src
 CLANG_SRC=${BASE}/llvm_src/tools/clang
 LLVMRT_SRC=${BASE}/llvm_src/projects/compiler-rt
 POLLY_SRC=${LLVM_SRC}/tools/polly
-POLLY_COMMIT="d24789b6ad270d80509c73fc986d1275895c5b7b"
 INTELOMPRT=${BASE}/intelomprt
 LLVM_BUILD=${BASE}/llvm_build
 mkdir -p ${LLVM_BUILD}
@@ -70,56 +122,44 @@ LLVM_DEP=${LLVM_BUILD}/dependencies
 mkdir -p ${LLVM_DEP}
 CLOOG_SRC=${LLVM_DEP}/cloog_src
 CLOOG_INSTALL=${LLVM_DEP}/cloog_install
-# INTELOMPRT_VERSION=20131209
 INTELOMPRT_VERSION=20140716
-INTELOMPRT_FILE=libomp_${INTELOMPRT_VERSION}_oss.tgz
+INTELOMPRT_FILE=https://www.openmprtl.org/sites/default/files/libomp_${INTELOMPRT_VERSION}_oss.tgz
 
 # Obtaining the sources
 
 # LLVM Sources
 echo
 echoc "Obtaining LLVM OpenMP..."
-if [ "$HTTP" == "true" ]; then
-    git clone https://github.com/clang-omp/llvm.git ${LLVM_SRC}
-else
-    git clone git@github.com:clang-omp/llvm.git ${LLVM_SRC}
-fi
+git clone ${LLVM_REPO} ${LLVM_SRC}
+cd ${LLVM_SRC}
+git reset --hard ${LLVM_COMMIT}
 
 # Clang Sources
 echo
 echoc "Obtaining LLVM/Clang OpenMP..."
-if [ "$HTTP" == "true" ]; then
-    git clone https://github.com/clang-omp/clang.git ${CLANG_SRC}
-else
-    git clone -b clang-omp git@github.com:clang-omp/clang.git ${CLANG_SRC}
-fi
+git clone ${CLANG_REPO} ${LLVM_SRC}
+cd ${CLANG_SRC}
+git reset --hard ${CLANG_COMMIT}
 
 # Runtime Sources
 echo
 echoc "Obtaining LLVM OpenMP Runtime..."
-if [ "$HTTP" == "true" ]; then
-    git clone https://github.com/clang-omp/compiler-rt.git ${LLVMRT_SRC}
-else
-    git clone git@github.com:clang-omp/compiler-rt.git ${LLVMRT_SRC}
-fi
+git clone ${LLVMRT_REPO} ${LLVMRT_SRC}
+cd ${LLVMRT_SRC}
+git reset --hard ${LLVMRT_COMMIT}
 
 # Polly Sources
 echo
 echoc "Obtaining Polly..."
-if [ "$HTTP" == "true" ]; then
-    git clone https://github.com/llvm-mirror/polly.git ${POLLY_SRC}
-else
-    git clone git@github.com:llvm-mirror/polly.git ${POLLY_SRC}
-fi
+git clone ${POLLY_REPO} ${POLLY_SRC}
 cd ${POLLY_SRC}
-# git checkout ${POLLY_COMMIT}
 git reset --hard ${POLLY_COMMIT}
 
 # Intel OpenMP Runtime Sources
 mkdir ${INTELOMPRT}
 echo
 echo "Obtaining Intel OpenMP Runtime..."
-wget --directory-prefix=${INTELOMPRT} https://www.openmprtl.org/sites/default/files/${INTELOMPRT_FILE}
+wget --directory-prefix=${INTELOMPRT} ${INTELOMPRT_FILE}
 
 # Applying the Patch
 
@@ -141,15 +181,15 @@ echoc "Patching Polly..."
 cd ${POLLY_SRC}
 patch -p 1 < ${WORKING_DIR}/patch/polly.patch
 
-# Intel OpenMP Runtime Patch
-echo
-echoc "Patching Intel OpenMP Runtime..."
-cd ${CLANG_SRC}
-patch -p 1 < ${WORKING_DIR}/patch/clang-intelomprt.patch
-cd ${LLVMRT_SRC}
-patch -p 1 < ${WORKING_DIR}/patch/compiler-rt-intelomprt.patch
-cd ${INTELOMPRT}
-tar xzvf ${INTELOMPRT_FILE}
+# # Intel OpenMP Runtime Patch
+# echo
+# echoc "Patching Intel OpenMP Runtime..."
+# cd ${CLANG_SRC}
+# patch -p 1 < ${WORKING_DIR}/patch/clang-intelomprt.patch
+# cd ${LLVMRT_SRC}
+# patch -p 1 < ${WORKING_DIR}/patch/compiler-rt-intelomprt.patch
+# cd ${INTELOMPRT}
+# tar xzvf ${INTELOMPRT_FILE}
 # patch -p 0 < ${WORKING_DIR}/patch/libomp_oss_${INTELOMPRT_VERSION}.patch
 
 # Compiling and installing Cloog (dependency for Polly)
@@ -157,7 +197,7 @@ echo
 echoc "Building dependencies..."
 ${POLLY_SRC}/utils/checkout_cloog.sh ${CLOOG_SRC}
 cd ${CLOOG_SRC}
-./configure --prefix=${CLOOG_INSTALL}
+CC=$(which gcc) CXX=$(which g++) ./configure --prefix=${CLOOG_INSTALL}
 make -j${PROCS} -l${PROCS}
 make install
 
@@ -169,9 +209,7 @@ echoc "Building LLVM/Clang OpenMP..."
 cp -R ${WORKING_DIR}/plugins/ArcherPlugin ${LLVM_SRC}/tools/clang/examples
 echo "add_subdirectory(ArcherPlugin)" >> ${LLVM_SRC}/tools/clang/examples/CMakeLists.txt
 cd ${LLVM_BUILD}
-export CC=gcc
-export CXX=g++
-cmake -D CMAKE_INSTALL_PREFIX:PATH=${LLVM_INSTALL} -D CMAKE_PREFIX_PATH=${CLOOG_INSTALL} -D LINK_POLLY_INTO_TOOLS:Bool=ON ${LLVM_SRC}
+CC=$(which gcc) CXX=$(which g++) cmake -D CMAKE_INSTALL_PREFIX:PATH=${LLVM_INSTALL} -D CMAKE_PREFIX_PATH=${CLOOG_INSTALL} -D LINK_POLLY_INTO_TOOLS:Bool=ON ${LLVM_SRC}
 make [REQUIRES_RTTI=1]* -j${PROCS} -l${PROCS}
 echoc "Building Archer Plugins..."
 make ArcherPlugin
@@ -183,7 +221,7 @@ make install
 echoc "Building Intel OpenMP Runtime..."
 cd ${INTELOMPRT}
 cd libomp_oss/cmake
-cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
+CC=$(which gcc) CXX=$(which g++) cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ ..
 make -j${PROCS} -l${PROCS}
 cp -r ${INTELOMPRT}/libomp_oss/exports/lin_32e/lib ${LLVM_INSTALL}/lib/intelomprt
 # Installing Instrumented Intel OpenMP Runtime (temporary until patch)
